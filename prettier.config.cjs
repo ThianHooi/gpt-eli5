709c894da299d228a4b2d80f4d7d71ae52bd1c77
ooithianhooi <ooithianhooi@gmail.com>
2023-04-04 22:31:53 +0800

    chore: prettier config

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,26 +1,7 @@
-const pluginSortImports = require('@trivago/prettier-plugin-sort-imports')
-const pluginTailwindcss = require('prettier-plugin-tailwindcss')
-
-/**
- * @refs  https://github.com/tailwindlabs/prettier-plugin-tailwindcss/issues/31#issuecomment-1195411734
- * @refs  https://github.com/trivago/prettier-plugin-sort-imports/issues/117#issuecomment-1198805533
- */
-/** @type {import("prettier").Parser}  */
-const bothParser = {
-  ...pluginSortImports.parsers.typescript,
-  parse: pluginTailwindcss.parsers.typescript.parse,
-}
-
-/** @type {import("prettier").Plugin}  */
-const mixedPlugin = {
-  parsers: {
-    typescript: bothParser,
-  },
-}
-
+/** @type {import("prettier").Config} */
 module.exports = {
-  plugins: [mixedPlugin],
-  importOrder: ['(^next$|^next/(.*)$)', '<THIRD_PARTY_MODULES>', '^~/(.*)$', '^[./]'],
-  importOrderSeparation: false,
-  importOrderSortSpecifiers: true,
-}
+  plugins: [
+    require("@trivago/prettier-plugin-sort-imports"),
+    require.resolve("prettier-plugin-tailwindcss"),
+  ],
+};
